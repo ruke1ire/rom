fc84cac0ca1b8d1d02a20e0503f27d4d85408310
Rom <rom.parnichkun@gmail.com>
2022-06-17 10:58:12 +0700

    riced bashrc

--- append_to_bashrc.sh
+++ append_to_bashrc.sh
@@ -4,4 +4,4 @@ if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
 fi
 
 #changes the prompt
-PS1=" \[\033[01;34m\]\w\[\033[00m\]\$ "
+PS1=" \[\$(if [ \$? == 0 ]; then echo -e \"\\e[0;92m\"; else echo -e \"\\e[0;91m\"; fi)\]●\[\e[0m\] \[\033[01;34m\]\w\[\033[00m\]\$ "
